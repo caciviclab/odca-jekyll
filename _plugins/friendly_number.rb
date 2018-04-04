1d71f97e4ecb153817c47777cbcc5c01933539e2
Aaron D Borden <adborden@a14n.net>
2018-04-03 19:47:14 -0700

    rfc
    - Use 1-line guard conditions
    - Avoid `$` global variables
    - Simplify negative formatting using `.negative?` and `.abs`

--- _plugins/friendly_number.rb
+++ _plugins/friendly_number.rb
@@ -1,33 +1,29 @@
-$unit = ['', 'K', 'M', 'B', 'T']
+unit = ['', 'K', 'M', 'B', 'T']
 
 module Jekyll
   module FriendlyNumberFilter
     # Formats a number with a unit 1750 => 1.7K
     def friendly_number(number)
-      unless number.is_a? Numeric
-        return number
-      end
+      return number unless number.is_a? Numeric
 
-      $iterations = 0
-      while number / 1000 >= 1 && $iterations < $unit.size do
+      iterations = 0
+      while number / 1000 >= 1 && iterations < unit.size do
         number = number / 1000.0
-        $iterations += 1
+        iterations += 1
       end
 
       # If number is less than 10, include a decimal
       if number < 10
-        '%.1f%s' % [number.round(1), $unit[$iterations]]
+        '%.1f%s' % [number.round(1), unit[iterations]]
       else
-        '%d%s' % [number.round, $unit[$iterations]]
+        '%d%s' % [number.round, unit[iterations]]
       end
     end
 
     # Adds thousands separator
     # https://codereview.stackexchange.com/a/28100
     def comma_number(number)
-      unless number.is_a? Numeric or number.is_a? String
-        return number
-      end
+      return number unless number.is_a? Numeric or number.is_a? String
 
       whole, decimal = number.to_s.split(".")
       whole_with_commas = whole.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
@@ -36,32 +32,18 @@ module Jekyll
 
     # Format as money with dollars and cents
     def money(number)
-      unless number.is_a? Numeric
-        return number
-      end
+      return number unless number.is_a? Numeric
 
-      $is_negative = number < 0
-      if $is_negative
-        number *= -1
-      end
-
-      $money = '$%s' % comma_number(number.round(2))
-      $is_negative ? '(%s)' % $money : $money
+      money = '$%s' % comma_number(number.abs.round(2))
+      number.negative? ? '(%s)' % money : money
     end
 
     # Shortcut for number | floor | comma_number | prepend:'$'
     def dollars(number)
-      unless number.is_a? Numeric
-        return number
-      end
+      return number unless number.is_a? Numeric
 
-      $is_negative = number < 0
-      if $is_negative
-        number *= -1
-      end
-
-      $money = '$%s' % comma_number(number.round)
-      $is_negative ? '(%s)' % $money : $money
+      money = '$%s' % comma_number(number.abs.round)
+      number.negative? ? '(%s)' % money : money
     end
   end
 end
