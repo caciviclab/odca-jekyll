508252ee6ff1772a9e0e670a25ae4dc7f40241d0
Aaron D Borden <adborden@a14n.net>
2018-07-29 19:18:31 -0700

    Remove Root placeholder

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,2 @@
-/* globals document */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root';
-
 // We need to include our components so they are included in the bundle
 import './components/contributions-table';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<Root />, root);
-});
