cae485b5df807aee034d227b17c0167165d95264
Aaron D Borden <adborden@a14n.net>
2018-08-07 20:19:39 -0700

    Update some old links and email addresses

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -11,7 +11,7 @@ cd _site
 git --version
 git init
 git config user.name "CA Civic Lab deploy script"
-git config user.email "opencal@googlegroups.com"
+git config user.email "caciviclab+deploy@googlegroups.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "git@github.com:${GITHUB_REPO}.git" master:gh-pages &> /dev/null || ( exit_code=$?; echo Error deploying to GH Pages: exit $exit_code >&2; exit $exit_code )
