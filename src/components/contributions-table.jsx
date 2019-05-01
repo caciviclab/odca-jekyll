1805c0e550eba2c85aeccab2d8f51309e55a599e
Tom Dooner <tomdooner@gmail.com>
2019-04-30 22:33:13 -0700

    Allow contribution search with more fields
    This adds support for searching the contribution list by employer,
    occupation, zip code, and amount. This is based on [Casey's feedback
    doc][1] of improvements for professional campaign finance researchers.
    
    [Finishes #307]
    [1]: https://docs.google.com/document/d/1vZj-u4fmc8EPMmAhdbJbdp-VgmS8vJBQukbbildJTI0/edit#

--- src/components/contributions-table.jsx
+++ src/components/contributions-table.jsx
@@ -76,7 +76,12 @@ class ContributionsTable extends React.Component {
 
     return contributions
       .filter((contribution) => {
-        const tokens = tokenize(contribution.name);
+        const tokens =
+          tokenize(contribution.name)
+          .concat(tokenize(contribution.employer))
+          .concat(tokenize(contribution.occupation))
+          .concat(contribution.zip)
+          .concat(String(contribution.amount));
         const queries = tokenize(filter);
 
         // Every query needs to match at least one token. It's considered a
