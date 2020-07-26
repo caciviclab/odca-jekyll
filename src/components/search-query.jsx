4e82ed60e409a370a60f2464fa3cb0c7715c8ff2
Michael Ubell <mike.ubell@gmail.com>
2020-07-26 15:12:42 -0700

    Fix review comments.

--- src/components/search-query.jsx
+++ src/components/search-query.jsx
@@ -7,29 +7,36 @@ import webComponent from '../web-component';
 
 const searchClient = algoliasearch('H897LKXYG1', '50a4f124e0d934cac92e79ece376316a');
 
-const hitComponent = ({ hit }) => (
+const HitComponent = ({ hit }) => (
   <div>
     { hit.last_name ?
-      <div><strong>Contributor:</strong> {hit.first_name} {hit.last_name}</div> : ''
+      <div>
+        <strong>Contributor:</strong> {hit.first_name} {hit.last_name}
+      </div>
+      : ''
     }
-    <div><strong>Election: </strong>
+    <div>
+      <strong>Election: </strong>
       <a href={`/election/${hit.election_location.toLowerCase()}/${hit.election_date}`} >
         {hit.election_title}
       </a>
     </div>
     { hit.title ?
-      <div><strong>Ballot Measure: </strong>
+      <div>
+        <strong>Ballot Measure: </strong>
         <a href={`/referendum/${hit.election_location.toLowerCase()}/${hit.election_date}/${hit.slug}`} >
           {hit.title}
         </a>
-      </div> : ''}
+      </div>
+      : ''}
     { hit.name ?
-      <div><strong>Candidate: </strong>
+      <div>
+        <strong>Candidate: </strong>
         <a href={`/candidate/${hit.election_location.toLowerCase()}/${hit.election_date}/${hit.candidate_slug}`} >
           {hit.name}
         </a>
       </div>
-     : ''
+      : ''
     }
     { hit.office_title ?
       <div>
@@ -39,12 +46,16 @@ const hitComponent = ({ hit }) => (
         </a>
       </div> : ''
     }
-    { hit.amount ? <div><strong>Amount:</strong> ${hit.amount}</div> : ''}
-    <strong>Election date:</strong> {hit.election_date}<br />
-    <strong>Location:</strong> {hit.election_location}<br />
+    { hit.amount ?
+      <div>
+        <strong>Amount:</strong> ${hit.amount}
+      </div>
+      : ''}
+    <div> <strong>Election date:</strong> {hit.election_date} </div>
+    <div> <strong>Location:</strong> {hit.election_location} </div>
   </div>
 );
-hitComponent.propTypes = {
+HitComponent.propTypes = {
   hit: PropTypes.shape({
     last_name: PropTypes.string,
     first_name: PropTypes.string,
@@ -60,28 +71,26 @@ hitComponent.propTypes = {
     office_title: PropTypes.string,
   }),
 };
-hitComponent.defaultProps = {
+HitComponent.defaultProps = {
   hit: {},
 };
 
-class SearchQuery extends React.Component {
-  render() {
-    return (
-      <InstantSearch searchClient={searchClient} indexName="election">
-        <div className="left-panel">
-          <ClearRefinements clearsQuery="true" />
-          <h3> Election Title </h3>
-          <RefinementList attribute="election_title" />
-        </div>
-        <div className="right-panel">
-          <SearchBox className="searchbar" />
-          <Hits hitComponent={hitComponent} />
-          <Pagination />
-          <a href="https://www.algolia.com" >Search by Algolia</a>
-        </div>
-      </InstantSearch>
-    );
-  }
-}
+const SearchQuery = () => (
+  <InstantSearch searchClient={searchClient} indexName="election">
+    <div className="grid">
+      <div className="grid-col-2 election-checkboxes">
+        <ClearRefinements clearsQuery="true" />
+        <h3>Election Title</h3>
+        <RefinementList attribute="election_title" />
+      </div>
+      <div className="grid-col-10">
+        <SearchBox className="searchbar" />
+        <Hits hitComponent={HitComponent} />
+        <Pagination />
+        <a href="https://www.algolia.com">Search by Algolia</a>
+      </div>
+    </div>
+  </InstantSearch>
+);
 
 export default webComponent(SearchQuery, 'search-query');
