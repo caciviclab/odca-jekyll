7b4524803c1c5e60977e1961d8e3e0998e84d9b3
Jeffrey Van <jeffvan159@gmail.com>
2020-08-11 19:34:06 -0700

    code style

--- src/components/search-query.jsx
+++ src/components/search-query.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import algoliasearch from 'algoliasearch';
 import PropTypes from 'prop-types';
 
-import { RefinementList, InstantSearch, SearchBox, ClearRefinements, Pagination,
-  connectHits, PoweredBy } from 'react-instantsearch-dom';
+import {
+  RefinementList, InstantSearch, SearchBox, ClearRefinements, Pagination,
+  connectHits, PoweredBy,
+} from 'react-instantsearch-dom';
 import webComponent from '../web-component';
 
 const searchClient = algoliasearch('H897LKXYG1', '50a4f124e0d934cac92e79ece376316a');
