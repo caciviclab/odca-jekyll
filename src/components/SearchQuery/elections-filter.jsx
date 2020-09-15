7758aed77fbf7625bfd050d2ac877a7bf4ec243c
anlawyer <anlawyer@berkeley.edu>
2020-09-14 17:45:04 -0700

    Clean up map + orderby methods into one line for refinements list

--- src/components/SearchQuery/elections-filter.jsx
+++ src/components/SearchQuery/elections-filter.jsx
@@ -6,16 +6,10 @@ import { date } from '../../format';
 const ElectionsFilter = () => (
   <RefinementList
     attribute="election_date"
-    transformItems={(items) => {
-        // Override items label value to show formatted date
-        const formattedItems = map(items, item => ({
+    transformItems={items => map(orderBy(items, i => i.label, 'desc'), item => ({
           ...item,
           label: date('MMMM D, YYYY', item.label),
-        }));
-        // Parse date to get UTC value and order the list of elections by it
-        return orderBy(formattedItems, i => Date.parse(i.label), 'desc');
-      }
-    }
+        }))}
   />
 );
 
